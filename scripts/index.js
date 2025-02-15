419f6419315f4954e9d0fc56c1d9ceeb5a227735
Enrique Tecuapetla <enriquettip@gmail.com>
2025-02-15 13:41:27 -0600

    feat (createCard) Se añade función para crear cartas

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [
+    {
+      name: "Parque Ecoturístico Dos Aguas",
+      link: "./images/Dos_Aguas.jpeg"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ];
 const editButton = document.querySelector(".info__edit-button");
 const popup = document.querySelector(".popup");
 const form = document.querySelector(".form");
@@ -7,6 +33,7 @@ const profileName = document.querySelector(".info__name");
 const profileAbout = document.querySelector(".info__about");
 const popupCloseButton = document.querySelector(".popup__close-button");
 const like = document.querySelectorAll(".card__like");
+const placesContainer = document.querySelector(".places");
 
 function enableModal() {
     formName.value=profileName.textContent;
@@ -35,14 +62,24 @@ form.addEventListener("submit",handleFormSubmit);
 popupCloseButton.addEventListener("click",function () {
     closeM();
 });
+
 like.forEach(element => {
     element.addEventListener("click", () => {
-        if(element.classList.contains("card__like_black")){
-            element.classList.remove("card__like_black");
-            console.log("quitar");
-        }else{
-            element.classList.add("card__like_black");
-            console.log("añadir");
-        }
+        element.classList.toggle("card__like_black")
     });
 });
+
+function createCard(imgURL, title) {
+    const template = document.querySelector("#template").content;
+    const card = template.querySelector(".card").cloneNode(true);
+    card.querySelector(".card__img").src=imgURL;
+    card.querySelector(".card__text").textContent = title;
+    card.querySelector(".card__like").addEventListener("click",function(evt){
+        evt.target.classList.toggle("card__like_black");
+    });
+    placesContainer.append(card);
+}
+initialCards.forEach(card => {
+    createCard(card.link,card.name);
+});
+//https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg
