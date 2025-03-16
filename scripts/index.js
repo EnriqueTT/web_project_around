ca3c4e90a1d852a6657cc9edfc89f71323c0db15
Enrique Tecuapetla <enriquettip@gmail.com>
2025-03-16 16:16:02 -0600

    feature (close popup) Se añade función para cerrar con Escape y se elimina el click en el botón cerrar

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 const initialCards = [
-    {
-      name: "Parque Ecoturístico Dos Aguas",
-      link: "./images/Dos_Aguas.jpeg"
-    },
-    {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
-    } 
-  ];
+  {
+    name: "Parque Ecoturístico Dos Aguas",
+    link: "./images/Dos_Aguas.jpeg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
 const editButton = document.querySelector(".info__edit-button");
 const popup = document.querySelector(".popup");
 const form = document.querySelector(".form");
@@ -40,35 +40,36 @@ const addForm = document.querySelector(".add-form");
 const popups = document.querySelectorAll(".popup");
 
 function enableModal() {
-    formName.value=profileName.textContent;
-    formAbout.value=profileAbout.textContent;
-    popup.classList.add("popup_opened");
-} 
+  formName.value = profileName.textContent;
+  formAbout.value = profileAbout.textContent;
+  popup.classList.add("popup_opened");
+}
 function closeM(popup) {
   popup.classList.remove("popup_opened");
 }
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    // console.log(formName.getAttribute("value"));NO SIRVE PORQUE 
-    // ESTE MÉTODO NO TRBAJA CON EL DOM
-    if (formName.value!="" && formAbout.value!=""){
-        profileName.textContent = formName.value;
-        profileAbout.textContent=formAbout.value;
-        // profileName.setAttribute("value",formName.value);
-        closeM(evt.target.closest(".popup"));
-    } else{
-        alert("campos vacíos");
-    }
+  evt.preventDefault();
+  // console.log(formName.getAttribute("value"));NO SIRVE PORQUE
+  // ESTE MÉTODO NO TRBAJA CON EL DOM
+  if (formName.value != "" && formAbout.value != "") {
+    profileName.textContent = formName.value;
+    profileAbout.textContent = formAbout.value;
+    // profileName.setAttribute("value",formName.value);
+    closeM(evt.target.closest(".popup"));
+  } else {
+    alert("campos vacíos");
+  }
 }
 
-editButton.addEventListener("click",enableModal);
-form.addEventListener("submit",handleFormSubmit);
-popupCloseButtons.forEach((closeButton) => {
-    closeButton.addEventListener("click",function(){
-    const targetPopup = closeButton.closest(".popup");
-    closeM(targetPopup);
-    })
-} );
+editButton.addEventListener("click", enableModal);
+form.addEventListener("submit", handleFormSubmit);
+//función para cerrar un popup con el botón de cerrar
+// popupCloseButtons.forEach((closeButton) => {
+//   closeButton.addEventListener("click", function () {
+//     const targetPopup = closeButton.closest(".popup");
+//     closeM(targetPopup);
+//   });
+// });
 
 function removeButtonHandler(card) {
   card.remove();
@@ -80,39 +81,43 @@ function pictureClickHandler(imgCard) {
   const imgPopUp = document.querySelector(".popup_img");
   imgPopUp.classList.add("popup_opened");
   imgPopUp.querySelector("img").src = imgCard.src;
-  imgPopUp.querySelector("p").textContent = imgCard.closest(".card").querySelector("p").textContent; 
-
+  imgPopUp.querySelector("p").textContent = imgCard
+    .closest(".card")
+    .querySelector("p").textContent;
 }
 
-function createCard( title , imgURL) {
-    if(title !== "" && imgURL !== ""){
-        const template = document.querySelector("#template").content;
-        const card = template.querySelector(".card").cloneNode(true);
-        const imgCard = card.querySelector(".card__img");
-        imgCard.src=imgURL;
-        card.querySelector(".card__text").textContent = title;
-        card.querySelector(".card__like").addEventListener("click",likeButtonHandler);
-        placesContainer.prepend(card);
-        card.querySelector(".card__img-trash")
-        .addEventListener("click",()=>removeButtonHandler(card));
-        imgCard.addEventListener("click",()=>pictureClickHandler(imgCard));
-    } else{
-        alert("campos vacíos");
-    }
+function createCard(title, imgURL) {
+  if (title !== "" && imgURL !== "") {
+    const template = document.querySelector("#template").content;
+    const card = template.querySelector(".card").cloneNode(true);
+    const imgCard = card.querySelector(".card__img");
+    imgCard.src = imgURL;
+    card.querySelector(".card__text").textContent = title;
+    card
+      .querySelector(".card__like")
+      .addEventListener("click", likeButtonHandler);
+    placesContainer.prepend(card);
+    card
+      .querySelector(".card__img-trash")
+      .addEventListener("click", () => removeButtonHandler(card));
+    imgCard.addEventListener("click", () => pictureClickHandler(imgCard));
+  } else {
+    alert("campos vacíos");
+  }
 }
 
-initialCards.forEach(({link,name}) => createCard(name,link));
-addButton.addEventListener("click",function(){
-    addPopUp.querySelector(".form__link").value="";
-    addPopUp.querySelector(".form__name").value="";
-    addPopUp.classList.add("popup_opened");
+initialCards.forEach(({ link, name }) => createCard(name, link));
+addButton.addEventListener("click", function () {
+  addPopUp.querySelector(".form__link").value = "";
+  addPopUp.querySelector(".form__name").value = "";
+  addPopUp.classList.add("popup_opened");
 });
 //evt.target es el elemento que activó el evento
-addForm.addEventListener("submit",function(evt){
-    evt.preventDefault();
-    const formInputs = evt.target.querySelectorAll("input");
-    createCard(formInputs[0].value,formInputs[1].value);
-    closeM(addForm.closest(".popup"));
+addForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const formInputs = evt.target.querySelectorAll("input");
+  createCard(formInputs[0].value, formInputs[1].value);
+  closeM(addForm.closest(".popup"));
 });
 //console.log(2*Math.pow(10,5));
 // initialCards.forEach(card => createCard(card.link,card.name));
@@ -124,11 +129,45 @@ addForm.addEventListener("submit",function(evt){
 //       console.log("click");
 //   }
 // })});
-
+//Función que cierra el popup con hacer click fuera de el (incluyendo botón x)
 popups.forEach((popup) => {
-  popup.addEventListener("click", function(evt){
-    if( evt.target.classList.contains("popup") ){
+  popup.addEventListener("click", function (evt) {
+    console.log(evt.target);
+    const targetClassList = evt.target.classList;
+    if (
+      targetClassList.contains("popup") ||
+      targetClassList.contains("popup__close-button")
+    ) {
       closeM(popup);
     }
   });
 });
+//Función para cerrar con botón Escape
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    popups.forEach((popup) => {
+      if (popup.classList.contains("popup_opened")) {
+        popup.classList.remove("popup_opened");
+      }
+    });
+  }
+});
+
+// document.forms.forEach((form) => {
+//   form.addEventListener("input", (evt) => {
+//     if (evt.target.classList.contains("form__input")) {
+//       if (!evt.target.validity.valid) {
+//         disableSubmitButton(form.querySelector(".form__submit-button"));
+//       } else {
+//         enableSubmitButton(form.querySelector(".form__submit-button"));
+//       }
+//     }
+//   });
+// });
+
+function disableSubmitButton(button) {
+  button.classList.add("form__submit-button_gray");
+}
+function enableSubmitButton(button) {
+  button.classList.remove("form__submit-button_gray");
+}
